005c0d96dc0a65928e885106db3a978975ef7206
darth_llamah <gen_scheisskopf@o2.pl>
2015-04-14 12:36:46 +0200

    PwrBtn integration with PowerCtrl
    - updated PowerCtrl circuit to use with PwrBtn
    - updated PowerCtrl code- power on is triggered using button, added led
    for easier calibration

--- PowerCtrl.ino
+++ PowerCtrl.ino
@@ -13,31 +13,39 @@ Instructions how to enable Attiny85 support in Arduino IDE: http://highlowtech.o
 #include <elapsedMillis.h>
 int lightPin=3; //photoresistor input pin (attiny 2)
 int outPin=0; //output pin (attiny 5)
+int buttonPin=4; //button input pin (attiny 3)
+int ledPin=2; //led output pin (attiny 7)
+boolean ledState=false;
 elapsedMillis timer0;
 boolean timedOut;
-#define interval 10000 //timeout in millisecond
+#define interval 10000 //timeout in millisecond- 10s
 #define reference 512 //light threshold value, ~2.5V
 
 void setup() {
 pinMode(outPin, OUTPUT);
-digitalWrite(outPin, LOW);
-timedOut = true;
+digitalWrite(outPin, LOW); //by default dc-dc converters are not enabled
+pinMode(buttonPin,INPUT);
+digitalWrite(buttonPin,HIGH); //enable internal pull-up resistor
+digitalWrite(ledPin,LOW); //led is off
+timedOut = true; //no need to check timer
 }
 
 void loop() {
   if (timedOut){
-    if (analogRead(lightPin)<reference){
-    digitalWrite(outPin,HIGH);
-    timedOut=false;
-    timer0 =0;
+    if (digitalRead(buttonPin)==0){ //button is pressed- see PowerCtrl schematics
+    digitalWrite(outPin,HIGH); //enable dc-dc converters
+    timedOut=false; 
+    timer0 =0; //reset timer0
     }
   }
-    else if (!timedOut){
+    else {
       if ((timer0<=interval)&&(analogRead(lightPin)<reference)){
-      timer0 =0;
+      timer0 =0; //when light is detected reset the timer
+      ledState = !ledState; //switch led state
+      digitalWrite(ledPin,ledState); //turn led on or off
   }
   else if (timer0>interval) {
-    digitalWrite(outPin,LOW);
+    digitalWrite(outPin,LOW); //light was not detected and time is over- disable dc-dc converters
     timedOut=true;
      }
   }
